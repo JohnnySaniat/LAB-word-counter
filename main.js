b04ff8c95c8826068472373a3bcd4e6584425256
Johnny Saniat <Jcsaniat@gmail.com>
2023-07-24 11:49:02 -0500

    got the word counter to function

--- main.js
+++ main.js
@@ -1,15 +1,12 @@
 console.log("Connected!")
 // YOU WILL MODIFY THIS FUNCTION TO GET THE PROGRAM TO WORK
 const wordCounter = (value) => {
-  // Type into the input and press submit, you will see the value you entered in your console
-  console.log(value);
-
   if (value) {
-    // COMPLETE THE LOGIC 
-    wordCount.innerHTML = `Word Count: 0`; 
+    wordCount.innerHTML = textarea.value.length;
+    let txt = textarea.value.trim();
+    wordCount.textContent = txt.split(/\s+/).filter((item) => item).length;
   } else {
-    // if the value is empty, set the error message value to "Please input text"
-    error.innerHTML = ""; // UPDATE THIS
+    error.innerHTML = "Please input text";
   }
 }
 
